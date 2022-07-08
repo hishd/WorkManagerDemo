f170b7a9dad4e9086bc381c03b160b630e9cfd03
Hishara Dilshan <hish2k15@gmail.com>
2022-07-08 23:45:43 +0530

    Restructured code. Added function to pass return data to the caller or the starter

--- app/src/main/java/com/hishd/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/hishd/workmanagerdemo/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.work.*
 import com.hishd.workmanagerdemo.workers.UploadWorker
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var workManager: WorkManager
 
     //Create the companion object to use the keys
     companion object {
@@ -18,34 +21,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.workManager = WorkManager.getInstance(applicationContext)
+
         findViewById<Button>(R.id.btnStart).setOnClickListener {
             setOneTimeWorkRequest()
         }
     }
 
-    private fun setOneTimeWorkRequest() {
-        val workManager = WorkManager.getInstance(applicationContext)
-
+    private fun setOneTimeWorkRequestWithConstants() {
         //Setting constraints to the work manager to start when the device is charging
         //Setting constraints to the work manager to start when the device is connected to internet
-//        val constraints = Constraints.Builder().apply {
-////            this.setRequiresCharging(true)
-//            this.setRequiredNetworkType(NetworkType.CONNECTED)
-//        }.build()
+        val constraints = Constraints.Builder().apply {
+//            this.setRequiresCharging(true)
+            this.setRequiredNetworkType(NetworkType.CONNECTED)
+        }.build()
 
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
+            .build()
+
+        startRequest(uploadRequest)
+    }
+
+    private fun setOneTimeWorkRequestWithInputData() {
         //Passing values to the worker object
         val data = Data.Builder().apply {
             this.putInt(KEY_COUNT_VALUE, 150)
         }.build()
 
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
-//            .setConstraints(constraints)
             .setInputData(data)
             .build()
-        workManager.enqueue(uploadRequest)
 
-        workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this) {
+        startRequest(uploadRequest)
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+
+        startRequest(uploadRequest)
+    }
+
+    private fun startRequest(request: WorkRequest) {
+        workManager.enqueue(request)
+        workManager.getWorkInfoByIdLiveData(request.id).observe(this) {
             findViewById<TextView>(R.id.textView).text = it.state.name
+
+            //Receive the data sent from worker object
+            if(it.state.isFinished) {
+                val data = it.outputData
+                val message = data.getString(UploadWorker.KEY_WORKER)
+                Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
--- app/src/main/java/com/hishd/workmanagerdemo/workers/UploadWorker.kt
+++ app/src/main/java/com/hishd/workmanagerdemo/workers/UploadWorker.kt
@@ -2,21 +2,43 @@ package com.hishd.workmanagerdemo.workers
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.hishd.workmanagerdemo.MainActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
-class UploadWorker(context: Context, params: WorkerParameters): Worker(context, params) {
+class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    //Create the keys to pass the results to the caller or the starter
+    companion object {
+        const val KEY_WORKER = "UploadWorker"
+    }
+
     override fun doWork(): Result {
         return try {
 
-            //Capture the data which is sent by the sender or the starter
-            val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE, 0)
+            val count: Int? = null
 
-            for(i in 0..count)
+            //Capture the data which is sent by the sender or the starter
+            if (inputData.hasKeyWithValueOfType(MainActivity.KEY_COUNT_VALUE, Int::class.java)) {
+                val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE, 0)
+            }
+
+            for (i in 0 until 600)
                 Log.i("UploadWorker", "Uploading ${i}")
 
-            Result.success()
+            //Sending the current date to the caller or starter
+            val dateFormat = SimpleDateFormat("dd/M/yyyy hh:mm:ss", Locale.ENGLISH)
+            val currentDate = dateFormat.format(Date())
+
+            val data = Data.Builder().apply {
+                this.putString(KEY_WORKER, currentDate.toString())
+            }.build()
+
+//            Result.success()
+            Result.success(data)
         } catch (e: Exception) {
             Result.failure()
         }
