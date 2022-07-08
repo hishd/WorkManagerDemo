d9191b7bc7a34640fd0b7ff8262a613cbbbec2fa
Hishara Dilshan <hish2k15@gmail.com>
2022-07-08 22:41:12 +0530

    Created sample One Time Work Request

--- app/src/main/java/com/hishd/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/hishd/workmanagerdemo/MainActivity.kt
@@ -2,10 +2,24 @@ package com.hishd.workmanagerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.hishd.workmanagerdemo.workers.UploadWorker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnStart).setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+        WorkManager.getInstance(applicationContext)
+            .enqueue(uploadRequest)
     }
 }
